f8b63b4cd14f0997d548bba8107d19727f46d62f
Elodie D <elodie.deflon@gmail.com>
2022-02-17 16:42:21 +0100

    Client fonctionnel avec addition
    A corriger pour faire fonctionner avec VehicleList

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+import zeep
 
 
 app = Flask(__name__)
 
+transport = zeep.Transport(cache=None)
+client = zeep.Client("http://localhost:8000/?wsdl", transport=transport)
+
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    answer1 = client.service.addition(1, 2)
+    answer2 = client.service.addition(3, 4)
+    answer3 = client.service.addition(5, 6)
+    result = [answer1, answer2, answer3]
+    return render_template('index.html', v_list=result)
+
+
 
