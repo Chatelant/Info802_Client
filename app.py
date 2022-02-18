28b309d6794680262800b0bd68377f18888b9edd
Elodie D <elodie.deflon@gmail.com>
2022-02-18 10:53:18 +0100

    Affichage info véhicule
    - Lors de la sélection d'un véhicule, affiche ses infos
    - Voir si besoin de récupérer les infos du véhicule lors du calcul du trajet (avec API REST)

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, request
 import zeep
-from zeep.helpers import serialize_object
 
 app = Flask(__name__)
 
+
+# Connexion au service SOAP
+# TODO : changer l'URL
 transport = zeep.Transport(cache=None)
 client = zeep.Client("http://localhost:8000/?wsdl", transport=transport)
 
 
+# Lancement de l'application
+# Le service SOAP fournit les noms des véhicules électriques
 @app.route("/")
 def home():
-    # result = client.service.get_vehicles()
-    result = client.service.get_vehicles_names()
-    print(result)
-
-    return render_template('index.html', v_list=result)
+    names = client.service.get_vehicles_names()
+    return render_template('index.html', v_list=names)
 
 
+# Retourne les informations d'une voiture à partir de son nom
+# Le service SOAP fournit les informations
+@app.route("/car_selected", methods=['POST'])
+def get_car_info():
+    car_name = request.data.decode('UTF-8')
+    car_info = client.service.get_vehicle_info(car_name)
 
+    return json.dumps(car_info)
+
+
+# cars = client.service.get_vehicles()
+# print(cars)
